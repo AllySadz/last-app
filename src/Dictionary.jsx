8cac9088492c34b57840a3435dd7088c1417cbd3
AllySadz <alexandria.sadzius@gmail.com>
2022-03-19 16:32:44 +1100

    file upload

---
+++ src/Dictionary.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState({});
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        <input type="submit" />
+      </form>
+      <Results results={results} />
+    </div>
+  );
+}
