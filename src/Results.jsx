ff11ea508f6d5461e7253becf13102cc0596e162
AllySadz <alexandria.sadzius@gmail.com>
2022-03-27 14:48:27 +1100

    Update Results.jsx

--- src/Results.jsx
+++ src/Results.jsx
@@ -16,6 +16,8 @@ export default function Results(props) {
                   <Phonetic phonetic={phonetic} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </section>
